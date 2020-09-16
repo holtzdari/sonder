da576e119a193fbd3a7d0982289b805ddb84d66a
“Darina <holtzdari@gmail.com>
2020-09-16 19:55:09 +0300

    edit post

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -12,3 +12,16 @@ class PostForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write something amazing!'}),
         }
+
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Create Title'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Create Title Tag'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write something amazing!'}),
+        }
